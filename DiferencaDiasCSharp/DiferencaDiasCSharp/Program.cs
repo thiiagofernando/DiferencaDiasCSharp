6458c7695acbb8900fec1754d910389ebf435776
Thiago Fernando <thiago.fernando@msn.com>
2019-05-10 21:12:15 -0400

    Criando Metodo que compara as datas

---
+++ DiferencaDiasCSharp/DiferencaDiasCSharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace DiferencaDiasCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculaDias();
+        }
+
+        public static void CalculaDias()
+        {
+            CultureInfo cult = new CultureInfo("pt-BR");
+            DateTime PrimeiraData = DateTime.Now;
+            DateTime SegundaData = DateTime.Now.AddDays(7);
+
+            Console.WriteLine("Primeira Data: " + PrimeiraData.ToString("dd/MM/yyyy", cult));
+            Console.WriteLine("Segunda Data: " + SegundaData.ToString("dd/MM/yyyy", cult));
+            Console.WriteLine("Diferença de Dias: " + (SegundaData - PrimeiraData).Days);
+            Console.ReadKey();
+        }
+    }
+}
